d9239eeb177400dce9ce78b7441e1ea1521aa44d
ManosKout <manos.koutoulak@gmail.com>
2022-02-22 13:29:16 +0100

    test for multiclass data

--- networks/loaders/data_loader.py
+++ networks/loaders/data_loader.py
@@ -5,13 +5,13 @@ from torchvision import transforms as T
 from torchvision.transforms import functional as F
 import pydicom as dicom
 import cv2 as cv
-from loaders.preprocessing import crop_and_pad, limiting_filter,normalize_intensity
+from preprocessing import crop_and_pad, limiting_filter,normalize_intensity
 
 class ImageFolder(data.Dataset):
-	def __init__(self, root,image_size=256,mode='train',augmentation_prob=0.4):
+	def __init__(self, root,image_size=256,mode='train',augmentation_prob=0.4, is_multiorgan = False):
 		"""Initializes image paths and preprocessing module."""
 		self.root = root
-		
+		self.is_multiorgan = is_multiorgan
 		# GT : Ground Truth
 		img_path = os.path.join(root,"image")
 		# self.GT_paths = root[:-1]+'_GT/'
@@ -31,7 +31,7 @@ class ImageFolder(data.Dataset):
 		"""Reads an image from a file and preprocesses it and returns."""
 		image_path = self.image_paths[index]
 		filename = os.path.basename(image_path)
-		GT_path = os.path.join(self.GT_paths, filename.split(".")[0] + 'mask.png')
+		GT_path = os.path.join(self.GT_paths, filename.split(".")[0] + '.png')
 		to_tensor = T.Compose([
 			T.ToTensor(),
 		])
@@ -43,9 +43,15 @@ class ImageFolder(data.Dataset):
 		GT = cv.imread(GT_path, cv.IMREAD_GRAYSCALE)
 		image = image/np.max(image)
 		image = image.astype(np.float32)
+
 		GT = GT/np.max(GT)
-		GT = GT > 0.5
-		GT = GT.astype(np.float32)		
+
+		if not self.is_multiorgan:
+			GT = GT > 0.5
+
+		GT = GT.astype(np.float32)	
+
+
 		# if self.mode == "test":
 		# Resize keeping the same geometry
 		image = crop_and_pad(image,self.image_size,display=False)
@@ -67,23 +73,28 @@ class ImageFolder(data.Dataset):
 		"""Returns the total number of font files."""
 		return len(self.image_paths)
 
-def get_loader(image_path, image_size, batch_size, num_workers=2, mode='train',augmentation_prob=0.4):
+def get_loader(image_path, image_size, batch_size, num_workers=2, mode='train',is_multiorgan=True, augmentation_prob=0.4):
 	"""Builds and returns Dataloader."""
 	
-	dataset = ImageFolder(root = image_path, image_size =image_size, mode=mode,augmentation_prob=augmentation_prob)
+	dataset = ImageFolder(root = image_path, image_size =image_size, mode=mode,augmentation_prob=augmentation_prob, is_multiorgan=is_multiorgan)
 	data_loader = data.DataLoader(dataset=dataset,
 								  batch_size=batch_size,
 								  shuffle=True,
 								  num_workers=num_workers)
-	return data_loader
+	# return data_loader
+	return dataset
 
-import matplotlib.pyplot as plt
-
-# path = "C:\\Users\\ek779475\\Documents\\Koutoulakis\\automatic_segmentation\\Dataset\\RECTUM\\validation"
-# dataload = get_loader(path,256,1)
-# image,mask = dataload.__getitem__(40)
+import matplotlib.pyplot as plt 
+path = "/Users/manoskoutoulakis/Desktop/Sample/016_PRO_pCT_CGFL_ok/results"
+dataload = get_loader(path,256,1,is_multiorgan=True)
+image,mask = dataload.__getitem__(55)
 # transforms = T.Compose([T.ToPILImage()])
-# image = image.data.cpu().detach().numpy().squeeze()
-# plt.imshow(image, cmap="gray")
-# plt.show()
+image = image.data.cpu().detach().numpy().squeeze()
+mask = mask.data.cpu().detach().numpy().squeeze()
+print (np.unique(mask))
+
+plt.imshow(image, cmap="gray")
+plt.imshow(mask, cmap="jet", alpha= 0.3 )
+
+plt.show()
 
