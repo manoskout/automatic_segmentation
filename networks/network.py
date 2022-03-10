f1d0197b29859464e4d1b801a610acf08ba5a4a1
Emmanouil KOUTOULAKIS <manos.koutoulak@gmail.com>
2022-03-10 12:22:27 +0100

    Test creating an slaveatt unet

--- networks/network.py
+++ networks/network.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
-
+import torchio as tio
 def init_weights(net, init_type='normal', gain=0.02):
     def init_func(m):
         classname = m.__class__.__name__
@@ -350,6 +350,139 @@ class AttU_Net(nn.Module):
 
         d1 = self.Conv_1x1(d2)
 
+        return d1
+
+class slave_conv_block(nn.Module):
+    def __init__(self,ch_in : int,ch_out : int, dropout : float= 0.) -> None:
+        super(slave_conv_block,self).__init__()
+        layers = [
+            nn.Conv2d(ch_in, ch_out, kernel_size=1,stride=1,padding=0,),
+            nn.LayerNorm(ch_out),
+            nn.ReLU(inplace=True),
+        ]
+        if dropout != 0.:
+            layers.append(nn.Dropout(p=dropout))
+        self.conv = nn.Sequential(*layers)
+
+
+
+    def forward(self,x : torch.Tensor)-> torch.Tensor:
+        x = self.conv(x)
+        return x
+
+class SlaveAttU_Net(nn.Module):
+    def __init__(self,img_ch: int=3, output_ch: int= 1, dropout: float = 0.)-> None:
+        super(SlaveAttU_Net,self).__init__()
+        
+        self.Maxpool = nn.MaxPool2d(kernel_size=2,stride=2)
+
+        self.Conv1 = conv_block(ch_in=img_ch,ch_out=64, dropout= dropout)
+        self.Conv2 = conv_block(ch_in=64,ch_out=128, dropout= dropout)
+        self.Conv3 = conv_block(ch_in=128,ch_out=256, dropout= dropout)
+        self.Conv4 = conv_block(ch_in=256,ch_out=512, dropout= dropout)
+        self.Conv5 = conv_block(ch_in=512,ch_out=1024, dropout= dropout)
+
+        self.SlaveConv5 = slave_conv_block(ch_in=2048,ch_out=1024, dropout= dropout)
+        self.SlaveConv4 = slave_conv_block(ch_in= 1024,ch_out=512, dropout= dropout)
+        self.SlaveConv3 = slave_conv_block(ch_in= 512, ch_out= 256, dropout= dropout)
+        self.SlaveConv2 = slave_conv_block(ch_in= 256,ch_out= 128, dropout= dropout)
+        self.SlaveConv1 = slave_conv_block(ch_in= 128,ch_out= 64, dropout= dropout)
+
+
+        self.Up5 = up_conv(ch_in=1024,ch_out=512, dropout=dropout)
+        self.Att5 = Attention_block(F_g=512,F_l=512,F_int=256)
+        self.Up_conv5 = conv_block(ch_in=1024, ch_out=512, dropout=dropout)
+
+        self.Up4 = up_conv(ch_in=512,ch_out=256, dropout=dropout)
+        self.Att4 = Attention_block(F_g=256,F_l=256,F_int=128)
+        self.Up_conv4 = conv_block(ch_in=512, ch_out=256, dropout=dropout)
+        
+        self.Up3 = up_conv(ch_in=256,ch_out=128, dropout=dropout)
+        self.Att3 = Attention_block(F_g=128,F_l=128,F_int=64)
+        self.Up_conv3 = conv_block(ch_in=256, ch_out=128, dropout=dropout)
+        
+        self.Up2 = up_conv(ch_in=128,ch_out=64, dropout=dropout)
+        self.Att2 = Attention_block(F_g=64,F_l=64,F_int=32)
+        self.Up_conv2 = conv_block(ch_in=128, ch_out=64, dropout=dropout)
+
+        self.Conv_1x1 = nn.Conv2d(64,output_ch,kernel_size=1,stride=1,padding=0)
+
+    def forward(self,x: torch.Tensor, sl: torch.Tensor) -> torch.Tensor:
+        # encoding path
+        x1 = self.Conv1(x)
+        sl1 = self.Conv1(sl)
+
+        x2 = self.Maxpool(x1)
+        x2 = self.Conv2(x2)
+        
+        x3 = self.Maxpool(x2)
+        x3 = self.Conv3(x3)
+
+        x4 = self.Maxpool(x3)
+        x4 = self.Conv4(x4)
+
+        x5 = self.Maxpool(x4)
+        x5 = self.Conv5(x5)
+
+        sl2 = self.Maxpool(sl1)
+        sl2 = self.Conv2(sl2)
+        
+        sl3 = self.Maxpool(sl2)
+        sl3 = self.Conv3(sl3)
+
+        sl4 = self.Maxpool(sl3)
+        sl4 = self.Conv4(sl4)
+
+        sl5 = self.Maxpool(sl4)
+        sl5 = self.Conv5(sl5)
+
+        # print(f"\nX1 size : {x1.shape}")
+        x1 = torch.cat((x1,sl1),dim=1) 
+        # print(f"\n [CAT] X1 size : {x1.shape}")
+
+        x2 = torch.cat((x2,sl2),dim=1) 
+        x3 = torch.cat((x3,sl3),dim=1) 
+        x4 = torch.cat((x4,sl4),dim=1) 
+        x5 = torch.cat((x5,sl5),dim=1) 
+        
+        x1= self.SlaveConv1(x1)
+        # print(f"X1 size : {x1.shape}")
+
+        x2= self.SlaveConv2(x2)
+        # print(f"x2 size : {x2.shape}")
+
+        x3= self.SlaveConv3(x3)
+        # print(f"x3 size : {x3.shape}")
+
+        x4= self.SlaveConv4(x4)
+        # print(f"x4 size : {x4.shape}")
+
+        x5= self.SlaveConv5(x5)
+        # print(f"x5 size : {x5.shape}")
+
+        # decoding + concat path
+        d5 = self.Up5(x5)
+        x4 = self.Att5(g=d5,x=x4)
+        d5 = torch.cat((x4,d5),dim=1)        
+        d5 = self.Up_conv5(d5)
+        # print(f"d5 size : {d5.shape}")        
+        d4 = self.Up4(d5)
+        x3 = self.Att4(g=d4,x=x3)
+        d4 = torch.cat((x3,d4),dim=1)
+        d4 = self.Up_conv4(d4)
+        # print(f"d4 size : {d4.shape}")
+        d3 = self.Up3(d4)
+        x2 = self.Att3(g=d3,x=x2)
+        d3 = torch.cat((x2,d3),dim=1)
+        d3 = self.Up_conv3(d3)
+        # print(f"d3 size : {d3.shape}")
+        d2 = self.Up2(d3)
+        x1 = self.Att2(g=d2,x=x1)
+        d2 = torch.cat((x1,d2),dim=1)
+        d2 = self.Up_conv2(d2)
+        # print(f"d2 size : {d2.shape}")
+        d1 = self.Conv_1x1(d2)
+        # print(f"d1 size : {d1.shape}")
         return d1
 
 
