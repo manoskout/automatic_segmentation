24b229d3747fa4c0d7f6652de0b22a34c47d6daf
Emmanouil KOUTOULAKIS <manos.koutoulak@gmail.com>
2022-03-22 11:51:30 +0100

    loss function update

--- networks/networks/losses.py
+++ networks/networks/losses.py
@@ -64,7 +64,8 @@ class DiceLoss(_Loss):
     def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
 
         assert y_true.size(0) == y_pred.size(0)
-
+        # y_true = y_tr.clone()
+        # y_pred = y_pr.clone()
         if self.from_logits:
             # Apply activations to get [0..1] class probabilities
             # Using Log-Exp as this gives more numerically stable result and does not cause vanishing gradient on
@@ -94,12 +95,14 @@ class DiceLoss(_Loss):
             if self.ignore_index is not None:
                 mask = y_true != self.ignore_index
                 y_pred = y_pred * mask.unsqueeze(1)
-
+                
                 y_true = F.one_hot((y_true * mask).to(torch.long), num_classes)  # N,H*W -> N,H*W, C
-                y_true = y_true.permute(0, 2, 1) * mask.unsqueeze(1)  # H, C, H*W
+                y_true = y_true.permute(0, 2, 1) * mask.unsqueeze(1)  # H, C, H*W                
             else:
                 y_true = F.one_hot(y_true, num_classes)  # N,H*W -> N,H*W, C
                 y_true = y_true.permute(0, 2, 1)  # H, C, H*W
+        
+                
 
         if self.mode == "multilabel":
             y_true = y_true.view(bs, num_classes, -1)
@@ -115,7 +118,7 @@ class DiceLoss(_Loss):
         if self.log_loss:
             loss = -torch.log(scores.clamp_min(self.eps))
         else:
-            loss = 1.0 - scores
+            loss = 1.0 - scores # Gamma 
 
         # Dice loss is undefined for non-empty classes
         # So we zero contribution of channel that does not have true pixels
@@ -135,3 +138,77 @@ class DiceLoss(_Loss):
 
     def compute_score(self, output, target, smooth=0.0, eps=1e-7, dims=None) -> torch.Tensor:
         return soft_dice_score(output, target, smooth, eps, dims)
+
+
+
+def soft_tversky_score(
+    output: torch.Tensor,
+    target: torch.Tensor,
+    alpha: float,
+    beta: float,
+    smooth: float = 0.0,
+    eps: float = 1e-7,
+    dims=None,
+) -> torch.Tensor:
+    assert output.size() == target.size()
+    if dims is not None:
+        intersection = torch.sum(output * target, dim=dims)  # TP
+        fp = torch.sum(output * (1.0 - target), dim=dims)
+        fn = torch.sum((1 - output) * target, dim=dims)
+    else:
+        intersection = torch.sum(output * target)  # TP
+        fp = torch.sum(output * (1.0 - target))
+        fn = torch.sum((1 - output) * target)
+
+    tversky_score = (intersection + smooth) / (intersection + alpha * fp + beta * fn + smooth).clamp_min(eps)
+    return tversky_score
+
+
+class TverskyLoss(DiceLoss):
+    """Tversky loss for image segmentation task.
+    Where TP and FP is weighted by alpha and beta params.
+    With alpha == beta == 0.5, this loss becomes equal DiceLoss.
+    It supports binary, multiclass and multilabel cases
+
+    Args:
+        mode: Metric mode {'binary', 'multiclass', 'multilabel'}
+        classes: Optional list of classes that contribute in loss computation;
+        By default, all channels are included.
+        log_loss: If True, loss computed as ``-log(tversky)`` otherwise ``1 - tversky``
+        from_logits: If True assumes input is raw logits
+        smooth:
+        ignore_index: Label that indicates ignored pixels (does not contribute to loss)
+        eps: Small epsilon for numerical stability
+        alpha: Weight constant that penalize model for FPs (False Positives)
+        beta: Weight constant that penalize model for FNs (False Positives)
+        gamma: Constant that squares the error function. Defaults to ``1.0``
+
+    Return:
+        loss: torch.Tensor
+
+    """
+
+    def __init__(
+        self,
+        mode: str,
+        classes: List[int] = None,
+        log_loss: bool = False,
+        from_logits: bool = True,
+        smooth: float = 0.0,
+        ignore_index: Optional[int] = None,
+        eps: float = 1e-7,
+        alpha: float = 0.7,
+        beta: float = 0.3,
+        gamma: float = 0.75,
+    ):
+
+        super().__init__(mode, classes, log_loss, from_logits, smooth, ignore_index, eps)
+        self.alpha = alpha
+        self.beta = beta
+        self.gamma = gamma
+
+    def aggregate_loss(self, loss):
+        return loss.mean() ** self.gamma
+
+    def compute_score(self, output, target, smooth=0.0, eps=1e-7, dims=None) -> torch.Tensor:
+        return soft_tversky_score(output, target, self.alpha, self.beta, smooth, eps, dims)
